d8498393d41f19fc8466973a4d0925fd443892a2
Emran Khandaker Evan <evanemran007@gmail.com>
2022-06-14 17:04:08 +0600

    lock method updated

--- app/src/main/java/com/example/locktank/LockScreenActivity.java
+++ app/src/main/java/com/example/locktank/LockScreenActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LockScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_lock_screen);
     }
 
---
+++ app/src/main/java/com/example/locktank/ThemeActivity.java
@@ -0,0 +1,14 @@
+package com.example.locktank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme);
+    }
+}
